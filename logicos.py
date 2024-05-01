c4504131eb2c4ac35fdf703f0eca0eb49e21c026
Gabriel Mendes <gabrielomendes02@gmail.com>
2024-05-01 15:35:19 -0300

    aprendendo sobre operadores lógicos

---
+++ logicos.py
@@ -0,0 +1,11 @@
+#and: retorna True se as duas condições forem verdadeiras
+10 > 2 and 7 > 3 #True
+
+#or: retorna True se uma das condições for verdadeira
+10 > 2 or 7 > 3 #True
+
+#not: retorna True se a condição for falsa
+not 10 > 2 #False
+
+#in: retorna True se o valor estiver presente na lista
+10 in [1,2,3,4,5] #False
